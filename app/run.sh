5d0e80953326a3a08204a3dfe0a2b31d45346f93
Arthur Uguen <87493045+artuguen28@users.noreply.github.com>
2024-03-22 12:48:36 +0100

    Testing bash script

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+sudo apt install gnome-terminal
 
-echo "Implemente aqui o script para executar a sua solução"
+# Run Flask backend
+echo "Starting Flask backend..."
+cd backend
+pip install -r requirements.txt
+cd api
+#python3 main.py
+gnome-terminal -- bash -c "python3 main.py; exec bash"
+cd ../..
+
+# Run Vue.js frontend
+echo "Starting Vue.js frontend..."
+cd frontend/tripfinderapp
+gnome-terminal -- bash -c "npm install && npm run serve; exec bash"
+#npm install
+#npm run serve
