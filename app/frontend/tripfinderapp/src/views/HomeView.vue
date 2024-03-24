6dfd0f82ffbd0ad0ef19b9f2a5f9bbe820ec030f
Arthur Uguen <87493045+artuguen28@users.noreply.github.com>
2024-03-24 17:03:39 +0100

    Merge pull request #2 from artuguen28/development
    Development

---
+++ app/frontend/tripfinderapp/src/views/HomeView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <div class="sidebar">
+      <img class="CB-logo" alt="CB Viagens logo" src="../assets/logo.png" />
+      <SidebarButton
+        class="sidebarButton"
+        @click="handleButtonClick"
+        :disabled="isDisabled"
+        buttonMsg="Calculadora de Viagem"
+      />
+    </div>
+    <div class="main">
+      <TripFinderComponent v-if="showTripForm" />
+      <WelcomeComponent v-else />
+    </div>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from "@/components/WelcomeComponent.vue";
+import SidebarButton from "@/components/SidebarButton.vue";
+import TripFinderComponent from "@/components/TripFinderComponent.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    WelcomeComponent,
+    SidebarButton,
+    TripFinderComponent,
+  },
+  data() {
+    return {
+      showTripForm: false,
+      isDisabled: false,
+    };
+  },
+  methods: {
+    handleButtonClick() {
+      this.showTripForm = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+}
+.CB-logo {
+  margin: 10px;
+  width: 5cm;
+}
+.main {
+  flex: 5;
+  background-color: white;
+}
+
+.sidebar {
+  flex: 1;
+  background-color: #2c3e50;
+  height: 100%;
+}
+</style>
